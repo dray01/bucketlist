d1bdf2ffacb1b1e1313dc4d77e90a3aa716a6276
dray01 <brett.drayton@gmail.com>
2020-11-07 11:34:23 +1100

    tabs

--- app.py
+++ app.py
@@ -50,10 +50,10 @@ def main():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        file = request.files['file']
+    file = request.files['file']
         extension = os.path.splitext(file.filename)[1]
     	f_name = str(uuid.uuid4()) + extension
-    	file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
         return json.dumps({'filename':f_name})
 
 @app.route('/showSignUp')
